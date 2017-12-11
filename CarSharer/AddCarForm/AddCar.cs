67c05436068e685a11faf05db3203f9a040f7e8c
DuoQueue <kim_frank@gmx.de>
2017-12-11 12:52:39 +0100

    New Layout for AddCar

---
+++ CarSharer/AddCarForm/AddCar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddCarForm
+{
+    public partial class AddCar : Form
+    {
+        public AddCar()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CarSharer/CarSharer/AddCar.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CarSharer
+{
+    partial class AddCar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brandLabel = new System.Windows.Forms.Label();
+            this.brandComboBox = new System.Windows.Forms.ComboBox();
+            this.modelLabel = new System.Windows.Forms.Label();
+            this.modelComboBox = new System.Windows.Forms.ComboBox();
+            this.pslabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // brandLabel
+            // 
+            this.brandLabel.AutoSize = true;
+            this.brandLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brandLabel.Location = new System.Drawing.Point(24, 62);
+            this.brandLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.brandLabel.Name = "brandLabel";
+            this.brandLabel.Size = new System.Drawing.Size(70, 25);
+            this.brandLabel.TabIndex = 0;
+            this.brandLabel.Text = "Brand:";
+            this.brandLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // brandComboBox
+            // 
+            this.brandComboBox.FormattingEnabled = true;
+            this.brandComboBox.Location = new System.Drawing.Point(98, 66);
+            this.brandComboBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.brandComboBox.Name = "brandComboBox";
+            this.brandComboBox.Size = new System.Drawing.Size(121, 21);
+            this.brandComboBox.TabIndex = 1;
+            // 
+            // modelLabel
+            // 
+            this.modelLabel.AutoSize = true;
+            this.modelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modelLabel.Location = new System.Drawing.Point(24, 97);
+            this.modelLabel.Name = "modelLabel";
+            this.modelLabel.Size = new System.Drawing.Size(72, 25);
+            this.modelLabel.TabIndex = 2;
+            this.modelLabel.Text = "Model:";
+            // 
+            // modelComboBox
+            // 
+            this.modelComboBox.FormattingEnabled = true;
+            this.modelComboBox.Location = new System.Drawing.Point(98, 101);
+            this.modelComboBox.Name = "modelComboBox";
+            this.modelComboBox.Size = new System.Drawing.Size(121, 21);
+            this.modelComboBox.TabIndex = 3;
+            // 
+            // pslabel
+            // 
+            this.pslabel.AutoSize = true;
+            this.pslabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pslabel.Location = new System.Drawing.Point(24, 132);
+            this.pslabel.Name = "pslabel";
+            this.pslabel.Size = new System.Drawing.Size(45, 25);
+            this.pslabel.TabIndex = 4;
+            this.pslabel.Text = "PS:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(98, 136);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // AddCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 371);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pslabel);
+            this.Controls.Add(this.modelComboBox);
+            this.Controls.Add(this.modelLabel);
+            this.Controls.Add(this.brandComboBox);
+            this.Controls.Add(this.brandLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.Name = "AddCar";
+            this.Padding = new System.Windows.Forms.Padding(20, 62, 20, 19);
+            this.Text = "AddCar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label brandLabel;
+        private System.Windows.Forms.ComboBox brandComboBox;
+        private System.Windows.Forms.Label modelLabel;
+        private System.Windows.Forms.ComboBox modelComboBox;
+        private System.Windows.Forms.Label pslabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
