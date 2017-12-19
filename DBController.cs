524be2bcc1080e8fff09fc78c414289ce19e243d
Robert Schöma <RobertSchoe@users.noreply.github.com>
2017-12-19 08:50:23 +0100

    update

--- DBController.cs
+++ DBController.cs
@@ -28,7 +28,6 @@ namespace CarSharer
             {
                 try
                 {
-                    
                     // open connection to database
                     con.Open();
                     // INTERACTION WITH DATABASE
